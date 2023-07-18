fd8bb0a34d3b12c68020fd1241f53f805f5c6d98
Дмитрий <duckduck717171@yahoo.com>
2023-07-18 05:36:45 +0300

    Added HQL, Updated Repositories and Controllers

--- src/main/java/com/example/springdatabasicdemo/repositories/OrderRepository.java
+++ src/main/java/com/example/springdatabasicdemo/repositories/OrderRepository.java
@@ -1,7 +1,9 @@
 package com.example.springdatabasicdemo.repositories;
+import com.example.springdatabasicdemo.models.Customer;
 import com.example.springdatabasicdemo.models.Desk;
 import com.example.springdatabasicdemo.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -19,4 +21,17 @@ public interface OrderRepository extends JpaRepository<Order, Integer>{
     List<Order> findByBarista_Id(Integer baristaId);
 
     List<Order> findByBaristaIdAndStatus(Integer baristaId, String status);
+    @Query("SELECT o FROM Order o")
+    List<Order> findAllOrders();
+    @Query("SELECT o FROM Order o WHERE o.status = :status")
+    List<Order> findByStatus(@Param("status") String status);
+    @Query("SELECT o FROM Order o WHERE o.customer = :customer")
+    List<Order> findByCustomer(@Param("customer") Customer customer);
+    @Modifying
+    @Query("UPDATE Order o SET o.status = :newStatus WHERE o.id = :id")
+    void updateOrderStatus(@Param("id") Integer id, @Param("newStatus") String newStatus);
+    @Modifying
+    @Query("DELETE FROM Order o WHERE o.id = :id")
+    void deleteOrderById(@Param("id") Integer id);
+
 }
